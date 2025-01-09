83339ae22e8ef2e85814d456330ed95c9f7dc26b
Jahongir <crazyout4@gmail.com>
2025-01-09 10:53:49 +0500

    Redesigned the startup and shutdown functionality and made it more modern within the framework

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from aiogram import types
 from config import TELEGRAM_BOT_TOKEN, NGROK_TUNNEL_URL
 from bot import bot, dp
@@ -7,20 +8,22 @@ from database import create_tables
 TELEBOT_URL = f'/bot/{TELEGRAM_BOT_TOKEN}'
 NGROK_FULL_URL = f'{NGROK_TUNNEL_URL}{TELEBOT_URL}'
 
-app = FastAPI()
-
-@app.on_event(event_type='startup')
-async def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_tables()
     webhook_info = await bot.get_webhook_info()
     if webhook_info.url != NGROK_FULL_URL:
         await bot.set_webhook(NGROK_FULL_URL)
+    print("Startup done")
+    yield
+    await bot.session.close()
+    print("Shutdown done")
+
+app = FastAPI(
+    lifespan=lifespan
+)
 
 @app.post(TELEBOT_URL)
 async def get_updates(update: dict):
     telegram_update = types.Update(**update)
     await dp.feed_update(bot=bot, update=telegram_update)
-
-@app.on_event(event_type='shutdown')
-async def on_shutdown():
-    await bot.session.close()
